67e4cd5018d6f975bae026d4f1c9de0b8a7bfba6
abudulrazaq841-ui <abudulrazaq841@gmail.com>
2026-09-26 01:25:16 +0100

    Create js/Supabase.js

---
+++ js/Supabase.js
@@ -0,0 +1,7 @@
+const SUPABASE_URL = "https://whdyyqbriaplvqhtdjpk.supabase.co";
+const SUPABASE_KEY = "sb_publishable_W0_7qAL8tpVu8_R8WV2tqw_XALW6t4q";
+
+const supabaseClient = window.supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+);
